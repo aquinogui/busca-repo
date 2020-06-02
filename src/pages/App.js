351ea9a6b5b11f0f6f093d7ba804e0dee686b542
unknown <guih_m@live.com>
2020-06-01 22:21:19 -0300

    subindo busca-repo

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
 import Routes from './routes';
 
 function App() {
---
+++ src/pages/Repositories/styled.js
@@ -0,0 +1,31 @@
+
+import styled from 'styled-components';
+
+export const Container = styled.div  `
+    width: 100%;
+    max-width: 991px;
+    margin: 0 auto;
+`
+
+export const Title = styled.h1 `
+    text-align:center;
+    font-size: rem;
+    font-weight:bold;
+    font-family: sans serif;
+    color: #333;
+`
+
+export const List = styled.ul `
+    list-style: none;
+    padding: 0;
+    font-family: sans serif;
+    
+`
+
+export const ListItem = styled.li `
+        margin: .5rem 0;
+        padding: .5rem;
+        -webkit-box-shadow: -2px 4px 91px 0px rgba(0,0,0,0.23);
+        -moz-box-shadow: -2px 4px 91px 0px rgba(0,0,0,0.23);
+        box-shadow: -2px 4px 91px 0px rgba(0,0,0,0.23);
+`
